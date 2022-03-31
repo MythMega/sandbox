0b88419f9172517fab874d11c963507ee872097c
MythMega <45936113+MythMega@users.noreply.github.com>
2022-03-31 11:58:47 +0200

    updated JMUtils.py

--- python_random_projects/JMUtils.py
+++ python_random_projects/JMUtils.py
@@ -2,21 +2,38 @@ import time
 
 # import only system from os
 from os import system, name
+import sys
+import random
+
+# 1 optional arg (double delay 'number of second before shutdowning program')
+# default value delay = 0
+# shutdown a program, with an optional delay
+def closeProgram(delay=0):
+    time.sleep(delay)
+    sys.exit()
+
 
 # 1 optional arg (double t 'number of seconds')
-# default value = 0.5
-# make the prog
+# default value t = 0.5;
+# make the prog wait
 def wait(t=0.5):
     time.sleep(t)
 
 
+# generate random number
+# 2 optional args (int min 'minimum' ; int max 'maximum')
+# defaults values min = 0 ; max = 1;
+def rng(min=0, max=1):
+    return random.randint(min, max)
+
+# Function = make that the user need to type enter to continue
 # 1 optional arg (bool c 'announce you need to press enter')
-# default value = 0.5
+# default value = False
 # if arg1 = True, announce that an entry is needed
 def waitInput(c=False):
     if c:
         print("\nPress enter to continue ...\n")
-    rep = input("")
+    input("")
 
 # usual typing error
 def retype():
@@ -30,6 +47,7 @@ def clearConsole():
 
 # really nice console cleaning, working for (I think) all OS
 def cleanClearConsole():
+		# for windowd(here, os.name is "nt")
     if name == 'nt':
         _ = system('cls')
 
