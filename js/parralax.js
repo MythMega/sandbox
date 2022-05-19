a0fe30f0019153a441780ae6eebbf436ca8f4f63
MythMega <45936113+MythMega@users.noreply.github.com>
2022-05-19 13:33:23 +0200

    Parralax JS : Doc

--- js/parralax.js
+++ js/parralax.js
@@ -6,7 +6,29 @@ function parallax(e){
 		const y = (window.innerHeight - e.pageY*speed)/20
 		layer.style.transform = `translateX(${x}px) translateY(${y}px)`})}
 /*
-To use this, you need to have 
+To use this, you need to have layer like this :
+<img src="layer_parralax.png" data-speed="1" class="layer">
+data-speed must be between -20 & 20. Negatives values will
+make the layer move inverted to mouse movement.
 
-
+all layer must be in a section with theses css attributes :
+<<
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+							>>
+								
+all layer in this section must be with theses css attributes :
+<<
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+						>>
 */
